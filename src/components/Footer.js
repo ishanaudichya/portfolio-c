f4361478b7a2d51ed059f527c2cf43e3adcb0b58
ishanaudichya <93155612+ishanaudichya@users.noreply.github.com>
2023-11-15 13:59:44 +0530

    x  svg update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import {
-  AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
-} from "react-icons/ai";
+import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
@@ -25,7 +21,7 @@ function Footer() {
               <a
                 href="https://github.com/ishanaudichya"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <AiFillGithub />
@@ -35,17 +31,24 @@ function Footer() {
               <a
                 href="https://twitter.com/ishan_audichya"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
-                <AiOutlineTwitter />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="0.9em"
+                  viewBox="0 0 512 512"
+                  fill="currentColor"
+                >
+                  <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"></path>
+                </svg>
               </a>
             </li>
             <li className="social-icons">
               <a
                 href="https://www.linkedin.com/in/ishan-audichya/"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <FaLinkedinIn />
@@ -55,7 +58,7 @@ function Footer() {
               <a
                 href="https://www.instagram.com/ishannnn.nn"
                 style={{ color: "white" }}
-                target="_blank" 
+                target="_blank"
                 rel="noopener noreferrer"
               >
                 <AiFillInstagram />
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -2,11 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import myImg from "../../Assets/pp.png";
 import Tilt from "react-parallax-tilt";
-import {
-  AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
-} from "react-icons/ai";
+import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Home2() {
@@ -19,14 +15,28 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-            I specialize in MERN stack development, with a strong focus on <b className="purple">React and Next.js</b> for creating modern web applications.<br/>
-            I have a keen interest in <b className="purple">Machine Learning</b> and <b className="purple">Deep Learning</b>, and I'm proficient in using TensorFlow. <br/>
-            My programming skills encompass <b className="purple">Python, C++, Java, and JavaScript </b><br/>
-When I have spare time, I enjoy <b className="purple">3D design</b> as a creative outlet primarily <b className="purple">Blender</b>.<br></br><br/>
-I'm committed to continuous learning and staying up-to-date with emerging technologies.<br/>
-My problem-solving mindset and dedication to coding excellence drive me to excel in my work.<br></br>
-I'm eager to collaborate with like-minded professionals and explore new opportunities in software development and artificial intelligence
-              
+              I specialize in MERN stack development, with a strong focus on{" "}
+              <b className="purple">React and Next.js</b> for creating modern
+              web applications.
+              <br />I have a keen interest in{" "}
+              <b className="purple">Machine Learning</b> and{" "}
+              <b className="purple">Deep Learning</b>, and I'm proficient in
+              using TensorFlow. <br />
+              My programming skills encompass{" "}
+              <b className="purple">Python, C++, Java, and JavaScript </b>
+              <br />
+              When I have spare time, I enjoy{" "}
+              <b className="purple">3D design</b> as a creative outlet primarily{" "}
+              <b className="purple">Blender</b>.<br></br>
+              <br />
+              I'm committed to continuous learning and staying up-to-date with
+              emerging technologies.
+              <br />
+              My problem-solving mindset and dedication to coding excellence
+              drive me to excel in my work.<br></br>
+              I'm eager to collaborate with like-minded professionals and
+              explore new opportunities in software development and artificial
+              intelligence
             </p>
           </Col>
           <Col md={4} className="myAvtar">
@@ -59,7 +69,14 @@ I'm eager to collaborate with like-minded professionals and explore new opportun
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
                 >
-                  <AiOutlineTwitter />
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="0.9em"
+                    viewBox="0 0 512 512"
+                    fill="currentColor"
+                  >
+                    <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"></path>
+                  </svg>
                 </a>
               </li>
               <li className="social-icons">
