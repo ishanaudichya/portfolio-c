2311720635a3148616a110498f2794567ac80f33
ishanaudichya <93155612+ishanaudichya@users.noreply.github.com>
2023-11-30 21:38:08 +0530

    add techstack

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,10 +2,8 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
 import Github from "./Github";
-import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
 import laptopImg from "../../Assets/about.png";
-import Toolstack from "./Toolstack";
 import Timeline from "./Timeline";
 
 function About() {
@@ -35,17 +33,7 @@ function About() {
             <img src={laptopImg} alt="about" className="img-fluid" />
           </Col>
         </Row>
-        <Timeline/>
-        <h1 className="project-heading">
-          Professional <strong className="purple">Skillset </strong>
-        </h1>
-       
-        <Techstack />
-
-        <h1 className="project-heading">
-          <strong className="purple">Tools</strong> I use
-        </h1>
-        <Toolstack />
+        <Timeline />
 
         <Github />
       </Container>
