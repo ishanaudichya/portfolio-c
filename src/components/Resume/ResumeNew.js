ee106a416dc4c24e870e21d0f46c969720c2379b
ishanaudichya <93155612+ishanaudichya@users.noreply.github.com>
2024-03-10 23:45:01 +0530

    resume add

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
-// import Button from "react-bootstrap/Button";
+import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-// import pdf from "../../Assets/../Assets/resume.pdf";
-// import { AiOutlineDownload } from "react-icons/ai";
-// import { Document, Page, pdfjs } from "react-pdf";
-// import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+import pdf from "../../Assets/resume.pdf";
+import { AiOutlineDownload } from "react-icons/ai";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
+  const [width, setWidth] = useState(900);
 
   useEffect(() => {
     setWidth(window.innerWidth);
@@ -21,42 +21,42 @@ function ResumeNew() {
         <Particle />
         <Row
           style={{
-            height: "78vh",
             justifyContent: "center",
             position: "relative",
           }}
         >
-          {/* <Button
+          <Button
             variant="primary"
-            href={"https://drive.google.com/uc?export=download&id=16R1uZtoJL6gmy9LTG0-mKbi4y3JR5VY9"}
+            href={
+              "https://drive.google.com/uc?export=download&id=1od5k3_6k39--mOH17ieVZL42dtVXW7FT"
+            }
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
-          
             <AiOutlineDownload />
-            &nbsp;Downlad Resume
-          </Button> */}
-          Resume is temporarily removed. Please drop me a message on any of my
-          socials for my resume
+            &nbsp;Download Resume
+          </Button>
         </Row>
 
-        {/* <Row className="resume">
+        <Row className="resume">
           <Document file={pdf} className="d-flex justify-content-center">
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document> 
-        </Row> */}
-        {/* 
+          </Document>
+        </Row>
+
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={'https://drive.google.com/uc?export=download&id=16R1uZtoJL6gmy9LTG0-mKbi4y3JR5VY9'}
+            href={
+              "https://drive.google.com/uc?export=download&id=1od5k3_6k39--mOH17ieVZL42dtVXW7FT"
+            }
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
             &nbsp;Downlad Resume
           </Button>
-        </Row> */}
+        </Row>
       </Container>
     </div>
   );
